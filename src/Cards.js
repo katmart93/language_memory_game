63574b7f88da0b775656f45e337bcf843671c82a
katmart93 <katwlodarczyk93@gmail.com>
2022-02-14 21:10:22 +0100

    imgs array created

--- src/Cards.js
+++ src/Cards.js
@@ -1,5 +1,56 @@
 import React from "react";
 
+const cardImgs = [
+  {
+    type: "cat",
+    src: "img/cat_card_DE.jpg",
+  },
+  {
+    type: "cat",
+    src: "img/cat_card_EN.jpg",
+  },
+  {
+    type: "dog",
+    src: "img/dog_card_DE.jpg",
+  },
+  {
+    type: "dog",
+    src: "img/dog_card_EN.jpg",
+  },
+  {
+    type: "guinea",
+    src: "img/guinea_pig_card_DE.jpg",
+  },
+  {
+    type: "guinea",
+    src: "img/guinea_pig_card_EN.jpg",
+  },
+  {
+    type: "hamster",
+    src: "img/hamster_card_DE.jpg",
+  },
+  {
+    type: "hamster",
+    src: "img/hamster_card_EN.jpg",
+  },
+  {
+    type: "rabbit",
+    src: "img/rabbit_card_DE.jpg",
+  },
+  {
+    type: "rabbit",
+    src: "img/rabbit_card_EN.jpg",
+  },
+  {
+    type: "rat",
+    src: "img/rat_card_DE.jpg",
+  },
+  {
+    type: "rat",
+    src: "img/rat_card_EN.jpg",
+  },
+];
+
 function Cards() {
   return <h1 className="test-header">Hello from the Cards component</h1>;
 }
