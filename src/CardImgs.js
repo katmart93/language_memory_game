ab14160c4fdb7d3c46debec2cc07224fe57918c3
katmart93 <katwlodarczyk93@gmail.com>
2022-02-15 19:57:31 +0100

    images component created

---
+++ src/CardImgs.js
@@ -0,0 +1,54 @@
+import React from "react";
+
+const cardImgs = [
+  {
+    type: "cat",
+    src: "/img/cat_card_DE.jpg",
+  },
+  {
+    type: "cat",
+    src: "/img/cat_card_EN.jpg",
+  },
+  {
+    type: "dog",
+    src: "/img/dog_card_DE.jpg",
+  },
+  {
+    type: "dog",
+    src: "/img/dog_card_EN.jpg",
+  },
+  {
+    type: "guinea",
+    src: "/img/guinea_pig_card_DE.jpg",
+  },
+  {
+    type: "guinea",
+    src: "/img/guinea_pig_card_EN.jpg",
+  },
+  {
+    type: "hamster",
+    src: "/img/hamster_card_DE.jpg",
+  },
+  {
+    type: "hamster",
+    src: "/img/hamster_card_EN.jpg",
+  },
+  {
+    type: "rabbit",
+    src: "/img/rabbit_card_DE.jpg",
+  },
+  {
+    type: "rabbit",
+    src: "/img/rabbit_card_EN.jpg",
+  },
+  {
+    type: "rat",
+    src: "/img/rat_card_DE.jpg",
+  },
+  {
+    type: "rat",
+    src: "/img/rat_card_EN.jpg",
+  },
+];
+
+export default cardImgs;
--- src/Cards.js
+++ src/Cards.js
@@ -1,56 +1,6 @@
 import React, { useState } from "react";
 import CardsGrid from "./CardsGrid";
-
-const cardImgs = [
-  {
-    type: "cat",
-    src: "/img/cat_card_DE.jpg",
-  },
-  {
-    type: "cat",
-    src: "/img/cat_card_EN.jpg",
-  },
-  {
-    type: "dog",
-    src: "/img/dog_card_DE.jpg",
-  },
-  {
-    type: "dog",
-    src: "/img/dog_card_EN.jpg",
-  },
-  {
-    type: "guinea",
-    src: "/img/guinea_pig_card_DE.jpg",
-  },
-  {
-    type: "guinea",
-    src: "/img/guinea_pig_card_EN.jpg",
-  },
-  {
-    type: "hamster",
-    src: "/img/hamster_card_DE.jpg",
-  },
-  {
-    type: "hamster",
-    src: "/img/hamster_card_EN.jpg",
-  },
-  {
-    type: "rabbit",
-    src: "/img/rabbit_card_DE.jpg",
-  },
-  {
-    type: "rabbit",
-    src: "/img/rabbit_card_EN.jpg",
-  },
-  {
-    type: "rat",
-    src: "/img/rat_card_DE.jpg",
-  },
-  {
-    type: "rat",
-    src: "/img/rat_card_EN.jpg",
-  },
-];
+import cardImgs from "./CardImgs";
 
 function Cards() {
   const [memoCards, setMemoCards] = useState([]);
