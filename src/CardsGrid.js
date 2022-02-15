77953d8bbe45e311dd169bc47c7defa9db0d541b
katmart93 <katwlodarczyk93@gmail.com>
2022-02-15 22:16:49 +0100

    stylesheet updated

--- src/CardsGrid.js
+++ src/CardsGrid.js
@@ -3,8 +3,8 @@ import React, { useState } from "react";
 function CardsGrid({ memoCards }) {
   return (
     <div className="cards-grid">
-      {memoCards.map((card) => (
-        <div className="single-card" key={card.id}>
+      {memoCards.map((card, idx) => (
+        <div className="single-card" key={idx}>
           <img className="card-front" src={card.src} />
         </div>
       ))}
