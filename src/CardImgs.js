b09ba6a24f4220034395f767a87bdf86a013473c
katmart93 <katwlodarczyk93@gmail.com>
2022-02-20 15:17:29 +0100

    match key added in CardImgs

--- src/CardImgs.js
+++ src/CardImgs.js
@@ -4,50 +4,62 @@ const cardImgs = [
   {
     type: "cat",
     src: "/img/cat_card_DE.jpg",
+    match: false,
   },
   {
     type: "cat",
     src: "/img/cat_card_EN.jpg",
+    match: false,
   },
   {
     type: "dog",
     src: "/img/dog_card_DE.jpg",
+    match: false,
   },
   {
     type: "dog",
     src: "/img/dog_card_EN.jpg",
+    match: false,
   },
   {
     type: "guinea",
     src: "/img/guinea_pig_card_DE.jpg",
+    match: false,
   },
   {
     type: "guinea",
     src: "/img/guinea_pig_card_EN.jpg",
+    match: false,
   },
   {
     type: "hamster",
     src: "/img/hamster_card_DE.jpg",
+    match: false,
   },
   {
     type: "hamster",
     src: "/img/hamster_card_EN.jpg",
+    match: false,
   },
   {
     type: "rabbit",
     src: "/img/rabbit_card_DE.jpg",
+    match: false,
   },
   {
     type: "rabbit",
     src: "/img/rabbit_card_EN.jpg",
+    match: false,
   },
   {
     type: "rat",
     src: "/img/rat_card_DE.jpg",
+    match: false,
   },
   {
     type: "rat",
     src: "/img/rat_card_EN.jpg",
+    match: false,
   },
 ];
 
