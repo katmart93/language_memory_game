3de68fdb541fd21299d3639dff4f1bd2e7f81df7
katmart93 <katwlodarczyk93@gmail.com>
2022-03-05 16:32:33 +0100

    ScoresList styles udated

--- src/ScoresList.js
+++ src/ScoresList.js
@@ -5,8 +5,12 @@ function ScoresList({ scores }) {
     <div className="scores-list">
       <div className="scores-list__container">
         <div className="scores-list__content">
+          <div className="scores-list__header">
+            <div>Score</div>
+            <div>Date</div>
+          </div>
           {scores.map((score, idx) => (
-            <div className="single-scores" key={idx}>
+            <div className="single-score" key={idx}>
               <div className="scores">{score.final} points</div>
               <div className="date">{score.date}</div>
             </div>
