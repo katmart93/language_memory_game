85d750e4851957c7ad0f85854ab188cc951f7c03
katmart93 <katwlodarczyk93@gmail.com>
2022-03-05 14:29:46 +0100

    styles added to Navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -4,14 +4,18 @@ import { NavLink } from "react-router-dom";
 function Navbar() {
   return (
     <div className="navbar">
-      <div className="logo">
-        <NavLink to="/">
-          Language<span>Memory</span>Game
-        </NavLink>
-      </div>
-      <div className="links">
-        <NavLink to="/">Home</NavLink>
-        <NavLink to="scores">Scores</NavLink>
+      <div className="navbar__container">
+        <div className="navbar__content">
+          <div className="logo">
+            <NavLink to="/">
+              Language<span>Memory</span>Game
+            </NavLink>
+          </div>
+          <div className="links">
+            <NavLink to="/">Home</NavLink>
+            <NavLink to="scores">Scores</NavLink>
+          </div>
+        </div>
       </div>
     </div>
   );
