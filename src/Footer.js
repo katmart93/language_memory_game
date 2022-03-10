5fced0833a7ddc6965876e212697aaab7056328d
katmart93 <katwlodarczyk93@gmail.com>
2022-03-10 18:15:19 +0100

    fontawesome icons installed, footer styles added

--- src/Footer.js
+++ src/Footer.js
@@ -1,11 +1,18 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
 
 function Footer() {
   return (
     <div className="footer">
       <div className="footer__container">
         <div className="footer__content">
-          <small>by katmart93</small>
+          <div className="icons">
+            <FontAwesomeIcon icon={faGithub} className="github-icon" />
+          </div>
+          <div className="copyright">
+            <small className="copyright-text">by katmart93</small>
+          </div>
         </div>
       </div>
     </div>
